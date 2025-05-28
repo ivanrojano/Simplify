f4e19a3993552185bb58754e025a6dd736dc5a74
ivanrojano <165648051+ivanrojano@users.noreply.github.com>
2025-05-28 12:29:27 +0200

    Update SecurityConfig.java

--- Backend/src/main/java/com/fctapp/servicios/security/SecurityConfig.java
+++ Backend/src/main/java/com/fctapp/servicios/security/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("https://simplify-mauve.vercel.app//"));
+        config.setAllowedOrigins(List.of("https://simplify-frotend.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
